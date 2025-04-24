dfc03ee1d9a595e96a16d3df1b7faf91818a5e9d
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-04-24 14:37:52 +0800

    Simplify `vercel.json` by removing unnecessary routes.
    The redundant routing definitions have been removed since the core functionality is still intact without them. This reduces configuration complexity and ensures better maintainability of the deployment settings.

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,23 +1,19 @@
-// lib/connectToDatabase.js
 import mongoose from "mongoose";
 import { DB_URI, NODE_ENV } from "../config/env.js";
 
-let isConnected = false;
-
 const connectToDatabase = async () => {
-  if (isConnected) return mongoose.connection;
+  if (mongoose.connection.readyState === 0) {
+    if (!DB_URI) {
+      throw new Error("Please define the MONGODB_URI environment variable.");
+    }
 
-  if (!DB_URI) {
-    throw new Error("Please define the MONGODB_URI environment variable.");
-  }
-
-  try {
-    await mongoose.connect(DB_URI);
-    isConnected = true;
-    console.log(`✅ MongoDB connected successfully in ${NODE_ENV} mode`);
-  } catch (error) {
-    console.error("❌ MongoDB connection error:", error);
-    throw error;
+    try {
+      await mongoose.connect(DB_URI);
+      console.log(`✅ MongoDB connected successfully in ${NODE_ENV} mode`);
+    } catch (error) {
+      console.error("❌ MongoDB connection error:", error);
+      throw error;
+    }
   }
 
   return mongoose.connection;
