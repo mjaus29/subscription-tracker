1ff476f004e98e4982610695abfc49a1082da1be
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-04-24 11:19:03 +0800

    Refactor API handler and add Vercel configuration.
    Modified the API handler to ensure the database connection is established only once per Lambda invocation. Added a `vercel.json` file to configure API rewrites for deployment on Vercel.

--- api/index.js
+++ api/index.js
@@ -2,6 +2,13 @@ import serverless from "serverless-http";
 import app from "../app.js";
 import connectToDatabase from "../database/mongodb.js";
 
-await connectToDatabase();
+let isConnected = false;
 
-export const handler = serverless(app);
+export const handler = async (event, context) => {
+  if (!isConnected) {
+    await connectToDatabase();
+    isConnected = true;
+  }
+  const handler = serverless(app);
+  return handler(event, context);
+};
